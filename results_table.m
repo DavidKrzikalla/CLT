f38a33026a50bb8a4ac2148752a4367ac2ac5db4
David <47105493+DavidKrzikalla@users.noreply.github.com>
2020-03-18 20:16:21 +0100

    Added some commenting:

--- results_table.m
+++ results_table.m
@@ -1,39 +1,45 @@
 %% This function creates results tables for stress and strain 
-
 % Author: David Krzikalla
 
-%Stress and strain table are created for general and material coordinate
+% Stress and strain tables are created for general and material coordinate
 %system
-%Finaly, those tables are combined with Z_coordinate of particular ply
-%boundary together with corresponding ply No.
+% Finaly, those tables are combined with Z_coordinate of particular ply
+%edge together with corresponding ply No.
 
-function [Eps_res, Sig_res]=results_table(Eps_xy,Eps_12,Sig_xy,Sig_12,z,angle)
+function [Eps_res, Sig_res,Z_coor]=results_table(Eps_xy,Eps_12,Sig_xy,Sig_12,z,angle)
 
-Eps_res_temp=zeros(length(angle)*2,6); %zero matrix (ply top, bottom vs ex,ey,gxy,e1,e2,g12)
+
+Eps_xy=round(Eps_xy,5);
+Sig_xy=round(Sig_xy,1);
+Eps_12=round(Eps_12,5);
+Sig_12=round(Sig_12,1);
+
+
+Eps_res=zeros(length(angle)*2,6); %zero matrix (ply top, bottom vs ex,ey,gxy,e1,e2,g12)
 
 j=1;
 while j<=length(angle)*2-1
     for i=length(angle):-1:1
         for k=1:3
-            Eps_res_temp(j,k)=Eps_xy(k,2,i);
-            Eps_res_temp(j,k+3)=Eps_12(k,2,i);
-            Eps_res_temp(j+1,k)=Eps_xy(k,1,i);
-            Eps_res_temp(j+1,k+3)=Eps_12(k,1,i);
+            Eps_res(j,k)=Eps_xy(k,2,i);
+            Eps_res(j,k+3)=Eps_12(k,2,i);
+            Eps_res(j+1,k)=Eps_xy(k,1,i);
+            Eps_res(j+1,k+3)=Eps_12(k,1,i);
         end
     j=j+2;
     end
 end
 
-Sig_res_temp=zeros(length(angle)*2,6); %zero matrix (ply top, bottom vs sx,sy,tauxy,s1,s2,tau12)
+Sig_res=zeros(length(angle)*2,6); %zero matrix (ply top, bottom vs sx,sy,tauxy,s1,s2,tau12)
 
 j=1;
 while j<=length(angle)*2-1
     for i=length(angle):-1:1
         for k=1:3
-            Sig_res_temp(j,k)=Sig_xy(k,2,i);
-            Sig_res_temp(j,k+3)=Sig_12(k,2,i);
-            Sig_res_temp(j+1,k)=Sig_xy(k,1,i);
-            Sig_res_temp(j+1,k+3)=Sig_12(k,1,i);
+            Sig_res(j,k)=Sig_xy(k,2,i);
+            Sig_res(j,k+3)=Sig_12(k,2,i);
+            Sig_res(j+1,k)=Sig_xy(k,1,i);
+            Sig_res(j+1,k+3)=Sig_12(k,1,i);
         end
     j=j+2;
     end
@@ -52,7 +58,8 @@ while j<=length(angle)*2-1
     end
 end
     
-Eps_res=[Z_coor Eps_res_temp];
-Sig_res=[Z_coor Sig_res_temp];
+Z_coor=round(Z_coor,4);
 
+Eps_res=[Z_coor Eps_res];
+Sig_res=[Z_coor Sig_res];
 end
