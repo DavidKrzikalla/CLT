c3d818fc72cbf1a16a02e2b1c285503ee80b98ad
David <47105493+DavidKrzikalla@users.noreply.github.com>
2020-05-20 12:06:58 +0200

    added mass calc, importing from excel

--- panel_stiffness_calculator.m
+++ panel_stiffness_calculator.m
@@ -1,23 +1,32 @@
 %% Sandwich panel bending stiffness calculator
+% Author: David Krzikalla
+
+% Calculation of panel flexural rigidity (EI) based on FSAE and FSG rules
+% Calculation of panel weights
+
+% Import data go from laminate_calc_theory_material_data.xlsx
+% Before calculation get familiar with calc procedure aspects in the excel
+
 clc
+%% Material import
+panel_mat=xlsread('laminate_calc_theory_material_data.xlsx','Stacking_sequence','D25:D31');
+
+baseline_tub=xlsread('laminate_calc_theory_material_data.xlsx','Stacking_sequence','D36:D38');
+
 %% Input data
 
-c=25; % Core thickness [mm]
-f1=0.66; % Upper face thickness [mm]
-f2=0.64; % Lower face thickness [mm]
-bf=200; % actual vehicle panel width [mm]
+f1=1.26; % Upper face thickness [mm]
+Ef1=54478; % Upper Face Young modulus [MPa]
+mf1=3612/2; % Upper face plane weight [g/m2]
 
-Ef1=46936; % Upper Face Young modulus [MPa]
-Ef2=40698; % Lower Face Young modulus [MPa]
-Ec=138; % Core Young modulus [MPa]
-Gc=48; % Core Shear modulus [MPa]
+f2=1.29; % Lower face thickness [mm]
+Ef2=58105; % Lower Face Young modulus [MPa]
+mf2=3612/2; % Lower face plane weight [g/m2]
+
+bf=200; % actual estimated vehicle panel width [mm]
+af=500; % actual estimated vehicle panel length [mm]
 
 RC= 3932; % Rig compliance [N/mm], get from SES based on steel tubes testing
-
-% Input requied baseline tubes for equivalence
-n=1; % no. of tubes required 
-DO=25.4; % Tube outer diameter [mm]
-wt=1.6; % wall thickness [mm]
 
 % %% Ec/Gc recalculation for isotropic core
 % % In case of isotropic core can be used:
@@ -29,6 +38,9 @@ wt=1.6; % wall thickness [mm]
 % % Known Gc [MPa]
 % Ec=Gc*(2*(1+muc)); 
 %% Calculation
+c=panel_mat(1,1); % Core thickness [mm]
+Ec=panel_mat(2,1); % Core Young modulus [MPa]
+Gc=panel_mat(3,1); % Core Shear modulus [MPa]
 L=400; % Minimal supports span [mm]
 b=275; % Width of testing panel [mm]
 h=c/2; % core half-thickness [mm]
@@ -47,6 +59,9 @@ EpanelRC=DpanelRC/((b*((c+f1+f2)^3-c^3))/12); % Panel Young's modulus corrected 
 Dpanelf=(EpanelRC*((bf*((c+f1+f2)^3-c^3))/12))/1000000; % [N*m2] 
 %% Baseline steel tubing bending stiffness
 % Calculation
+n=baseline_tub(1,1); % no. of tubes required 
+DO=baseline_tub(2,1); % Tube outer diameter [mm]
+wt=baseline_tub(3,1); % wall thickness [mm]
 Esteel=2e5; %YM of steel [MPa]
 DI=DO-(2*wt); % Inner diameter [mm] 
 I=(pi/64)*(DO^4-DI^4); % Second moment of inertia [mm^4]
@@ -54,11 +69,24 @@ EItube=(Esteel*I*n)/1000000; % bending stiffness baseline tubes [N*m2]
 
 %% Comparison 
 fprintf('Total panel thickness %.2f mm \n',c+f1+f2);
-fprintf('Panel %.0f Nm^2, baseline tubes %.0f Nm^2 \n',Dpanelf, EItube);
 
 if Dpanelf>=EItube
-    fprintf('OK, %.0f %% \n',Dpanelf/(EItube/100));
+    fprintf('Panel EI %.0f Nm^2, baseline tubes EI %.0f Nm^2 --> OK, %.0f %% \n',Dpanelf, EItube,Dpanelf/(EItube/100));
 else
-    fprintf('NOT ok, %.0f %% \n',Dpanelf/(EItube/100));
+    fprintf('Panel %.0f Nm^2, baseline tubes %.0f Nm^2 --> NOT ok, %.0f %% \n',Dpanelf, EItube,Dpanelf/(EItube/100));
 end
-    
+
+%% Panel mass estimation
+mfaces=mf1+mf2; % faces plane weight[g/m2]
+
+% Panel plain weight - mass of 1x1m panel 
+M_plane=round(mfaces+panel_mat(7,1)*c,0); %  panel plane mass [g]
+fprintf('Panel plane mass %.0f g/m^2 \n',M_plane);
+
+% Specimen mass (thickness x 275 x 500 mm)
+M_spec=round(M_plane*(0.275*0.5),0); % specimen panel mass [g]
+fprintf('Specimen mass %.0f g \n',M_spec);
+
+% Vehicle panel estimated weight
+M_veh=round(M_plane*((bf/1000)*(af/1000)),0); % specimen panel mass [g]
+fprintf('Estimated vehicle panel mass %.0f g \n',M_veh);
